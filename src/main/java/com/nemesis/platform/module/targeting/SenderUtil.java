ba555f81cd0f8e6d38b628b092fa3cd66cac0cc6
petar.tahchiev <paranoiabla@gmail.com>
2017-06-03 15:29:31 +0300

    Initial commit.

---
+++ src/main/java/com/nemesis/platform/module/targeting/SenderUtil.java
@@ -0,0 +1,37 @@
+package com.nemesis.platform.module.targeting;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * A utility class to send messages.
+ *
+ * @author Petar Tahchiev
+ * @since 1.5
+ */
+public class SenderUtil {
+
+    /**
+     * Send the given object to a queue with the given name.
+     *
+     * @param object    the object to send.
+     * @param queueName the name of the queue to which we send the object.
+     * @throws Exception
+     */
+    public static void sendToQueue(Object object, String queueName) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(queueName, false, false, false, null);
+        channel.basicPublish("", queueName, null, object.toString().getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + object.toString() + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
